9abf92d3ab289f1ac62797d7d10b55cb3bd9225c
0x-jerry <x.jerry.wang@gmail.com>
2021-08-10 12:36:47 +0800

    feat: add EventEmitter

---
+++ src/EventEmitter.test.ts
@@ -0,0 +1,71 @@
+import { EventEmitter } from './EventEmitter'
+
+type Events = {
+  test(): void
+  foo(a: number): void
+  bar(a: number, b: string): void
+}
+
+describe('EventEmitter', () => {
+  it('on', () => {
+    const ee = new EventEmitter<Events>()
+
+    ee.on('bar', (a) => {
+      expect(a).toBe(12)
+    })
+
+    ee.on('bar', (_, b) => {
+      expect(b).toBe('123')
+    })
+
+    ee.emit('bar', 12, '123')
+  })
+
+  it('once', () => {
+    const ee = new EventEmitter<Events>()
+
+    const fn = (x: number) => expect(x).toBe(12)
+
+    ee.once('foo', fn)
+    expect(Reflect.get(fn, EventEmitter.SymbolOnce)).toBe(true)
+
+    ee.emit('foo', 12)
+    expect(Reflect.get(fn, EventEmitter.SymbolOnce)).toBe(undefined)
+
+    expect(ee.events('foo').size).toBe(0)
+  })
+
+  it('off', () => {
+    const ee = new EventEmitter<Events>()
+
+    const fn = () => {}
+    ee.once('test', () => {})
+    ee.once('test', fn)
+
+    ee.once('foo', fn)
+
+    ee.once('bar', fn)
+
+    expect(ee.events('test').size).toBe(2)
+    expect(ee.events('foo').size).toBe(1)
+    expect(ee.events('bar').size).toBe(1)
+
+    ee.off('test', fn)
+    expect(ee.events('test').has(fn)).toBe(false)
+
+    ee.off('test')
+    expect(ee.events('test').size).toBe(0)
+
+    ee.off()
+    expect(Object.keys(ee.events()).length).toBe(0)
+  })
+
+  it('limit', () => {
+    const ee = new EventEmitter<Events>(1)
+
+    ee.on('test', () => {})
+
+    expect(() => ee.on('test', () => {})).toThrow('Listeners reached limit size: 1')
+    expect(() => ee.once('test', () => {})).toThrow('Listeners reached limit size: 1')
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './isObject'
 export * from './sleep'
 export * from './dom/index'
+export * from './EventEmitter'
