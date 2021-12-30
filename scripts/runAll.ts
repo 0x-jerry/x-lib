6707cc8387ad7a03a764e6115e3b4917d3507d1f
0x-jerry <x.jerry.wang@gmail.com>
2021-12-30 16:21:52 +0800

    chore: support append params

--- scripts/runAll.ts
+++ scripts/runAll.ts
@@ -8,22 +8,30 @@ const packagesDir = path.join(process.cwd(), 'packages')
 main(process.argv.slice(2))
 
 async function main(params: string[]) {
-  const [command, project = ''] = params
+  const [command, project = '', ...args] = params
 
   assert(command, 'Invalid command, please select one of [test, build, release]')
 
   const projectNames = await fs.readdir(packagesDir)
 
-  if (projectNames.includes(project)) {
-    const packageCwd = path.join(packagesDir, project)
-    await runInProject(command, packageCwd)
-
-    return
+  const runConf = {
+    projects: [] as string[],
+    params: [] as string[],
   }
 
-  for (const projectName of projectNames) {
+  if (projectNames.includes(project)) {
+    runConf.projects = [project]
+    runConf.params = args
+  } else {
+    runConf.projects = projectNames
+    runConf.params = [project, ...args]
+  }
+
+  for (const projectName of runConf.projects) {
     const packageCwd = path.join(packagesDir, projectName)
-    await runInProject(command, packageCwd)
+
+    const cmd = [command, ...runConf.params].join(' ')
+    await runInProject(cmd, packageCwd)
   }
 }
 
