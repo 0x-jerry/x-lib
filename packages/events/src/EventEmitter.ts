c38a9214a44f36152e019c1302a92d395bda8f53
0x-jerry <x.jerry.wang@gmail.com>
2022-01-06 14:43:31 +0800

    perf(events): add some comment

--- packages/events/src/EventEmitter.ts
+++ packages/events/src/EventEmitter.ts
@@ -15,6 +15,13 @@ export class EventEmitter<Events extends Record<string, ListenerFunction>> {
   #listeners: Listeners<Events>
   #limit: number
 
+  /**
+   * Limit count of listeners for every event.
+   */
+  get limit() {
+    return this.#limit
+  }
+
   constructor(limit = 20) {
     this.#limit = limit
     this.#listeners = {}
@@ -26,7 +33,14 @@ export class EventEmitter<Events extends Record<string, ListenerFunction>> {
     }
   }
 
+  /**
+   * Get all events and it's listeners.
+   */
   events<K extends keyof Events>(): Listeners<Events>
+  /**
+   * Get all listeners of the event.
+   * @param event Event type
+   */
   events<K extends keyof Events>(event: K): NonNullable<Listeners<Events>[K]>
   events<K extends keyof Events>(event?: K): Listeners<Events> | NonNullable<Listeners<Events>[K]> {
     if (!event) {
@@ -40,6 +54,12 @@ export class EventEmitter<Events extends Record<string, ListenerFunction>> {
     return this.#listeners[event]!
   }
 
+  /**
+   * Add a callback to the specified event.
+   * @param event Event type
+   * @param listener Callback
+   * @returns Remove the callback.
+   */
   on<K extends keyof Events>(event: K, listener: Events[K]): () => void {
     const events = this.events(event)
 
@@ -47,11 +67,16 @@ export class EventEmitter<Events extends Record<string, ListenerFunction>> {
 
     events.add(listener)
 
-    return () => {
-      this.off(event, listener)
-    }
+    return () => this.off(event, listener)
   }
 
+  /**
+   * Add a callback to the specified event, only execute once.
+   *
+   * @param event Event type
+   * @param listener Callback
+   * @returns Remove the callback.
+   */
   once<K extends keyof Events>(event: K, listener: Events[K]) {
     const events = this.events(event)
 
@@ -59,23 +84,49 @@ export class EventEmitter<Events extends Record<string, ListenerFunction>> {
 
     listener[Once] = true
     events.add(listener)
+
+    return () => this.off(event, listener)
   }
 
-  off<K extends keyof Events>(event?: K, listener?: Events[K]) {
+  /**
+   * Remove all listeners of all events.
+   */
+  off<K extends keyof Events>(): boolean
+  /**
+   *
+   * Remove all listeners of the event.
+   * @param event Event type
+   */
+  off<K extends keyof Events>(event: K): boolean
+  /**
+   * Remove the listener of the event.
+   *
+   * @param event Event type
+   * @param listener  Callback
+   * @returns Return true if listener is existed, otherwise return false.
+   */
+  off<K extends keyof Events>(event: K, listener: Events[K]): boolean
+  off<K extends keyof Events>(event?: K, listener?: Events[K]): boolean {
     if (!event) {
       this.#listeners = {}
-      return
+      return true
     }
 
     if (!listener) {
       delete this.#listeners[event]
-      return
+      return true
     }
 
     const events = this.events(event)
-    events.delete(listener)
+    return events.delete(listener)
   }
 
+  /**
+   * Trigger the event by event type.
+   *
+   * @param event Event type
+   * @param args Arguments that apply to the callback.
+   */
   emit<K extends keyof Events>(event: K, ...args: Parameters<Events[K]>) {
     const events = this.events(event)
     const clears: Events[K][] = []
